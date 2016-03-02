23379f590c84e1d4afab0583ff0d50912d0b0849
carbon <standardprogram@foxmail.com>
2016-03-02 16:01:15 +0800

    Could receive location

--- Assets/Scripts/Scene/WorldMapScene.cs
+++ Assets/Scripts/Scene/WorldMapScene.cs
@@ -1,82 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class WorldMapScene : BaseScene {
-
-
-	#region Mono
-	// Use this for initialization
-	void Start () {
-		Debug.Log("WorldMapScene Start");
-
-		InitUI ();
-		LoadMapTexture ();
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	#endregion
-
-
-
-	#region UI
-
-	private void InitUI(){
-
-
-	}
-
-
-	private void OnButtonClick() {
-
-	}
-
-
-
-	
-
-	#endregion
-
-
-	#region MAP
-	private void LoadMapTexture() {
-		GameObject obj = GameObject.FindGameObjectWithTag ("WorldMap");
-
-
-		StartCoroutine (MapNet.Instance.DownloadMapTexture (108.977541f,34.170067f, obj));
-	}
-
-
-
-	private void OnMapLoaded() {
-
-	}
-
-	private void OnFingerInput() {
-	
-	}
-
-	private void RotateMap() {
-	
-	}
-
-
-	private void ZoomMap() {
-
-	}
-
-	#endregion
-
-
-
-	#region Bastion
-
-	private void InitBastions() {
-		
-	}
-	#endregion
-}
+﻿using UnityEngine;using System.Collections;public class WorldMapScene : BaseScene {	// Use this for initialization	void Start () {		Debug.Log("WorldMapScene Start");	}		// Update is called once per frame	void Update () {		}}
